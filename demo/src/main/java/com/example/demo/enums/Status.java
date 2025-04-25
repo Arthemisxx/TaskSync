154ef77db4e2ebbc59f3ea65577edf749ed8df9a
nataliakalita <nkalita497@gmail.com>
2025-04-25 12:29:33 +0200

    init

--- demo/src/main/java/com/example/demo/enums/Status.java
+++ demo/src/main/java/com/example/demo/enums/Status.java
@@ -1,27 +1,12 @@
 package com.example.demo.enums;
 
 public enum Status {
-    TO_DO("to do"),
-    IN_PROGRESS("in progress"),
-    DONE("done");
+    TO_DO,
+    IN_PROGRESS,
+    DONE;
 
-    private final String dbValue;
 
-    Status(String dbValue) {
-        this.dbValue = dbValue;
-    }
 
-    @Override
-    public String toString() {
-        return dbValue;
-    }
 
-    public static Status fromString(String value) {
-        for (Status s : Status.values()) {
-            if (s.dbValue.equalsIgnoreCase(value)) {
-                return s;
-            }
-        }
-        throw new IllegalArgumentException("Unknown status: " + value);
-    }
+
 }
